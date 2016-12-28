c2f40f2c023f90889bb8077d9c50608595df4ed9
Christian Lemke <lemkech@gmail.com>
2016-12-28 17:36:47 +0100

    Update settings.py

--- DirtyDrive/settings.py
+++ DirtyDrive/settings.py
@@ -148,27 +148,26 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
+  'version': 1,
+  'disable_existing_loggers': False,
+  'filters': {
+    'require_debug_false': {
+      '()': 'django.utils.log.RequireDebugFalse'
     }
+  },
+  'handlers': {
+    'logfile': {
+      'class': 'logging.handlers.WatchedFileHandler',
+      'filename': 'D:/home/site/wwwroot/error.log'
+    },
+  },
+  'loggers': {
+    'django': {
+      'handlers': ['logfile'],
+      'level': 'ERROR',
+      'propagate': False,
+    },
+  }
 }
 
 # Specify the default test runner.
