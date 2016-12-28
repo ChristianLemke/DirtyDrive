a189fa60d28d35fcc4fdca4d687330ed56175bef
Christian Lemke <lemkech@gmail.com>
2016-12-28 17:16:58 +0100

    Update settings.py

--- DirtyDrive/settings.py
+++ DirtyDrive/settings.py
@@ -135,7 +135,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DirtyDrive.apps.DirtyDriveConfig',
-    #'app',
+    'DirtyDrive',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
